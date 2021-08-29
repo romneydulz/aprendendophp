e616e53c43cc31735d71c3eb549fed085e81f3f5
Romney de Matos Dulz <romneydulz@live.com>
2021-08-29 15:31:11 -0300

    correção variavel sobrenome

--- index.php
+++ index.php
@@ -19,8 +19,8 @@
         //isset($nome) retorna true or false caso a variavel esteja definida
         
         $nome = "Romney";
-        #sobrenome = "Dulz";
-        echo $nome."".$sobrenome;
+        $sobrenome = "Dulz";
+        echo $nome." ".$sobrenome;
         echo "<br/>";
         $anoNascimento = "18/11/1990";
         echo $anoNascimento;
