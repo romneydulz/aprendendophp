4455ec0675a0cb5a96496ab16e46ad489efb7982
Romney de Matos Dulz <romneydulz@live.com>
2021-08-29 23:34:19 -0300

    up

--- index.php
+++ index.php
@@ -1,48 +1,47 @@
 <!DOCTYPE html>
 <html lang="pt-br">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
 
     <?php
 
-        /*
+    /*
         Comentario de varias linhas
         */
-        echo "Hello World!";
-        echo "<br/>";
+    echo "Hello World!";
+    echo "<br/>";
 
-        /////////////////////////////////////////////////////////////////////////////
-        // testes com variaveis
-        //unset($nome) apaga variavel
-        //isset($nome) retorna true or false caso a variavel esteja definida
-        /////////////////////////////////////////////////////////////////////////////
-        
-        $nome = "Romney";
-        $sobrenome = "Dulz";
-        echo $nome." ".$sobrenome;
-        echo "<br/>";
-        $nascimento = new DateTime();
-        echo "<br/>";
+    /////////////////////////////////////////////////////////////////////////////
+    // testes com variaveis
+    //unset($nome) apaga variavel
+    //isset($nome) retorna true or false caso a variavel esteja definida
+    /////////////////////////////////////////////////////////////////////////////
 
-        //Exemplo de array
-        $frutas = array("Abacaxi", "Laranja", "Manga");
-        echo $frutas[1];
+    $nome = "Romney";
+    $sobrenome = "Dulz";
+    echo $nome . " " . $sobrenome;
+    echo "<br/>";
+    $nascimento = new DateTime();
+    echo "<br/>";
 
-        //var_dump() informa o tipo da variavel ou objeto
+    //Exemplo de array
+    $frutas = array("Abacaxi", "Laranja", "Manga");
+    echo $frutas[1];
 
-        $arquivo = fopen(".gitignore","r");
+    //var_dump() informa o tipo da variavel ou objeto
 
-        //var_dump($arquivo);
+    $arquivo = fopen(".gitignore", "r");
 
-
+    //var_dump($arquivo);
 
     ?>
-
-    </body>
+</body>
 
 </html>
